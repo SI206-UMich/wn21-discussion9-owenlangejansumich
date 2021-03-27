2782ba3c50df45da767b9dd06a5c52b8446e491c
owenlangejansumich <owenlang@umich.edu>
2021-03-27 14:29:51 -0400

    Final Commit

--- diss_9.py
+++ diss_9.py
@@ -6,33 +6,52 @@ import unittest
 # Task 1: Get the URL that links to the Pokemon Charmander's webpage.
 # HINT: You will have to add https://pokemondb.net to the URL retrieved using BeautifulSoup
 def getCharmanderLink(soup):
-    pass
+    # Find generation 1 Pokemon div tag
+    gen1 = soup.find(id = "gen-1").next_sibling.next_sibling
+    # Find the div tag with charmander
+    div_charmander = gen1.find_all('div', class_ = "infocard")[3]
+    # Find the span tag where the link is located
+    text_charmander = div_charmander.find('span', class_ = "infocard-lg-data text-muted")
+    # Find the link for charmander
+    link = text_charmander.find('a', class_ = "ent-name").get('href')
+
+    return "https://pokemondb.net" + link
 
 # Task 2: Get the details from the box below "Egg moves". Get all the move names and store
 #         them into a list. The function should return that list of moves.
 def getEggMoves(pokemon):
     url = 'https://pokemondb.net/pokedex/'+pokemon
-    #add code here
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+
+    parent_id = soup.find(id = "tab-moves-18")
+
+    div_egg_moves = [i for i in parent_id.find_all('h3') if i.text == "Egg moves"][0].next_sibling.next_sibling.next_sibling.next_sibling
+
+    return [i.find('td').text for i in div_egg_moves.find('tbody').find_all('tr')]
+
 
 # Task 3: Create a regex expression that will find all the times that have these formats: @2pm @5 pm @10am
 # Return a list of these times without the '@' symbol. E.g. ['2pm', '5 pm', '10am']
 def findLetters(sentences):
     # initialize an empty list
-    
+    l = []
 
     # define the regular expression
-    
+    reg_exp = "@(1?\d(?: pm|pm|am))"
 
     # loop through each sentence or phrase in sentences
-    
+    for sentence in sentences:
+        # find all the words that match the regular expression in each sentence
+        words = re.findall(reg_exp, sentence)
 
-    # find all the words that match the regular expression in each sentence
-       
-
-    # loop through the found words and add the words to your empty list
+        # loop through the found words and add the words to your empty list
+        for word in words:
+            l.append(word)
 
 
     #return the list of the last letter of all words that begin or end with a capital letter
+    return l
 
 
 
